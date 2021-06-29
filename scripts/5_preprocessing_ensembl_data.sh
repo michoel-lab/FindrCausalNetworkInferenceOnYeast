4cbc1abc52269fc763c86415096a5f61cd6f8500
adluinf <57450752+adluinf@users.noreply.github.com>
2021-06-29 16:21:58 +0200

    Update 5_preprocessing_ensembl_data.sh
    Bugfix.

--- scripts/5_preprocessing_ensembl_data.sh
+++ scripts/5_preprocessing_ensembl_data.sh
@@ -11,8 +11,8 @@ out1=cleaned_genes_ensembl83_yeast_step1.txt
 out2=cleaned_genes_ensembl83_yeast_step2.txt
 
 sed -n -e '/gene/{p;n;}' $inpath$file1 | sed -e 's/;/ /g' -e 's/description.*/ /' > $outpath$out1
-sed  -e 's/:/ /g' -e "s/[[:space:]]\+/ /g" $outpath$out1 > $outpath$out2
-#sed -n  '/ID=gene/p' $outpath$out1 | sed -n  '/Name/p'   > $outpath$out2
+#sed  -e 's/:/ /g' -e "s/[[:space:]]\+/ /g" $outpath$out1 > $outpath$out2
+sed -n  '/ID=gene/p' $outpath$out1 | sed -n  '/Name/p'   > $outpath$out2
 
 echo '# Done'
 # EOF
